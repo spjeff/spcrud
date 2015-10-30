361461c79758c89ad13d53eda194934194084810
Jeff Jones <spjeff@spjeff.com>
2015-10-30 13:13:31 -0500

    Code
    jsonRead() and jsonWrite() added for SPList to store raw JSON objects

--- spcrud-demo.js
+++ spcrud-demo.js
@@ -40,6 +40,27 @@ function spcrudCtl($scope, $http) {
     vm.done = function (operation) {
         vm.status = operation + ' complete ' + (new Date()).toTimeString();
     };
+
+    //JSON blob read
+    vm.settingsRead = function () {
+        spcrud.jsonRead($http, 'JSON-Settings').then(function (item) {
+            if (item) {
+                //success
+                console.log('settingsRead');
+                console.log(item);
+                vm.settings = item.JSON;
+            }
+        });
+    };
+
+    //JSON blob write
+    vm.settingsWrite = function () {
+        spcrud.jsonWrite($http, 'JSON-Settings', vm.settings).then(function (response) {
+            //response
+            console.log('settingsWrite');
+            console.log(response);
+        });
+    };
 }
 
 //load
--- spcrud.js
+++ spcrud.js
@@ -1,9 +1,31 @@
-//library with AngularJS operations for CRUD to SharePoint 2013 over REST api
+/**
+* Library with AngularJS operations for CRUD operations to SharePoint 2013 lists over REST api
+*
+* Contains 6 core functions
+*
+* 1) Create    - add item to List
+* 2) Read      - find all items or single item from List
+* 3) Update    - update item in List
+* 4) Delete    - delete item in List
+* 5) jsonRead  - save JSON to List
+* 6) jsonWrite - read JSON to List
+*
+* NOTE - 5 and 6 require the target SharePoint List to have two columns: "Title" (indexed) and "JSON" (mult-text).   These are
+* intendend to save JSON objects for JS internal application needs.   For example, saving user preferences to a "JSON-Settings" list
+* where one row is created per user (Title = current user Login) and JSON multi-text field holds the blob.  Simple and flexible to save data
+* for many scenarios.
+*
+* @spjeff
+* spjeff@spjeff.com
+* http://spjeff.com
+*
+* last updated 10-30-2015
+*/
 
 //namespace
 var spcrud = spcrud || {};
 
-//globals
+//configuration
 spcrud.apiUrl = _spPageContextInfo.webAbsoluteUrl + '/_api/web/lists/getbytitle(\'{0}\')/items';
 spcrud.json = 'application/json;odata=verbose';
 spcrud.headers = {
@@ -12,7 +34,7 @@ spcrud.headers = {
     'X-RequestDigest': document.querySelector('#__REQUESTDIGEST').value
 };
 
-//CREATE item
+//CREATE item - needs $http factory, SharePoint list name, and JS object to stringify for save
 spcrud.create = function ($http, listName, jsonBody) {
     //append metadata
     jsonBody['__metadata'] = {
@@ -29,7 +51,7 @@ spcrud.create = function ($http, listName, jsonBody) {
     return $http(config);
 };
 
-//READ entire list
+//READ entire list - needs $http factory and SharePoint list name
 spcrud.read = function ($http, listName) {
     var config = {
         method: 'GET',
@@ -39,7 +61,7 @@ spcrud.read = function ($http, listName) {
     return $http(config);
 };
 
-//READ single item
+//READ single item - needs $http factory, SharePoint list name, and item ID number
 spcrud.readItem = function ($http, listName, id) {
     var config = {
         method: 'GET',
@@ -49,7 +71,7 @@ spcrud.readItem = function ($http, listName, id) {
     return $http(config);
 };
 
-//UPDATE item
+//UPDATE item - needs $http factory, SharePoint list name, item ID number, and JS object to stringify for save
 spcrud.update = function ($http, listName, id, jsonBody) {
     //append HTTP header MERGE for UPDATE scenario
     var headers = JSON.parse(JSON.stringify(spcrud.headers));
@@ -71,7 +93,7 @@ spcrud.update = function ($http, listName, id, jsonBody) {
     return $http(config);
 };
 
-//DELETE item
+//DELETE item - needs $http factory, SharePoint list name and item ID number
 spcrud.del = function ($http, listName, id) {
     //append HTTP header DELETE for DELETE scenario
     var headers = JSON.parse(JSON.stringify(spcrud.headers));
@@ -86,25 +108,81 @@ spcrud.del = function ($http, listName, id) {
     return $http(config);
 };
 
-//parse SharePoint list item Id
+//parse SharePoint item ID from generic HTTP response
 spcrud.getId = function (resp) {
     var item;
-    if (resp.data.d) {
-        if (resp.data.d.results) {
-            //EDIT - nest JSON deeper in a 'results' object
-            if (resp.data.d.results.constructor === Array) {
-                item = resp.data.d.results[0];
-            } else {
-                item = resp.data.d.results;
-            }
+    if (resp.data.d.results) {
+        //EDIT - nest JSON deeper in a 'results' object
+        if (resp.data.d.results.constructor === Array) {
+            item = resp.data.d.results[0];
         } else {
-            //READ - flatter JSON response format
-            if (resp.data.d.constructor === Array) {
-                item = resp.data.d[0];
-            } else {
-                item = resp.data.d;
-            }
+            item = resp.data.d.results;
+        }
+    } else {
+        //READ - flatter JSON response format
+        if (resp.data.d.constructor === Array) {
+            item = resp.data.d[0];
+        } else {
+            item = resp.data.d;
         }
     }
     return (item) ? item.Id : null;
+};
+
+//lookup SharePoint Current User
+spcrud.getCurrentUser = function ($http) {
+    var url = _spPageContextInfo.webAbsoluteUrl + '/_api/web/currentuser';
+    var config = {
+        method: 'GET',
+        url: url,
+        cache: true,
+        headers: spcrud.headers
+    };
+    return $http(config);
+};
+
+//JSON blob read from SharePoint list - needs $http factory and SharePoint list name
+spcrud.jsonRead = function ($http, listName) {
+    return spcrud.getCurrentUser($http).then(function (response) {
+        //login name
+        spcrud.login = response.data.d.LoginName;
+
+        //GET SharePoint list
+        var config = {
+            method: 'GET',
+            url: spcrud.apiUrl.replace('{0}', listName) + '?$select=JSON,Id,Title&$filter=Title+eq+\'' + spcrud.login + '\'',
+            headers: spcrud.headers
+        };
+
+        //clean JSON response to SPListItem only
+        return $http(config).then(function (response) {
+            if (response.data.d.results) {
+                return response.data.d.results[0];
+            } else {
+                return null;
+            }
+        });
+    });
+};
+
+//JSON blob write to SharePoint list - needs $http factory, SharePoint list name, and JS object to stringify for save
+spcrud.jsonWrite = function ($http, listName, jsonBody) {
+    return spcrud.jsonRead($http, listName).then(function (item) {
+        //HTTP 200 OK
+        if (item) {
+            //update if found
+            item.JSON = JSON.stringify(jsonBody);
+            return spcrud.update($http, listName, item.Id, item);
+        } else {
+            //create if missing
+            var item = {
+                '__metadata': {
+                    'type': 'SP.ListItem'
+                },
+                'Title': spcrud.login,
+                'JSON': JSON.stringify(jsonBody)
+            };
+            return spcrud.create($http, listName, item);
+        }
+    });
 };
